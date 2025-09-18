cc3ec3beefdf22ed540eca5c058269c8d98d12c8
ankit-4623 <ankitnayek007@gmail.com>
2025-09-19 02:42:37 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT
 
 
 
-app.use(cors());
+app.use(cors({
+  origin:" http://localhost:5173",
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1",  photoRoutes);
