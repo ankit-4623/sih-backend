f5b383eec0101862cc7f414136f7e5e43413b580
ankit-4623 <ankitnayek007@gmail.com>
2025-09-19 09:40:31 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import photoRoutes from "./route/photo.js";
-
+import morgan from "morgan";
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT
@@ -21,11 +22,11 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use("/api/v1",  photoRoutes);
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
